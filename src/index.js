b1be3b901111894a8d674bc8c15d9caa60e966a5
karthikeyan2001-cyber <karthikeyan.e0219005@sret.edu.in>
2023-06-17 23:55:05 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,7 @@ import 'firebase/compat/firestore'; // Add Firestore import
 import 'firebase/compat/auth'; // Add Auth import
 import App from './App'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB08LL4gMSzxYtWNbZO-F-rfHGHzfbRPt0",
-//   authDomain: "proj-tracker-8776e.firebaseapp.com",
-//   projectId: "proj-tracker-8776e",
-//   storageBucket: "proj-tracker-8776e.appspot.com",
-//   messagingSenderId: "370402248328",
-//   appId: "1:370402248328:web:cb38b369faccad4246013d",
-//   measurementId: "G-PDT0VK04ZE"
-// };
-// firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

ad7929c4c2e25c27bf18caac0ac67bd90cb603d8
karthikeyan2001-cyber <karthikeyan.e0219005@sret.edu.in>
2023-06-21 23:36:06 +0530

    FormComponent.js

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FormComp.css';
 
-import {collection,doc,setDoc} from 'firebase/firestore';
-import { auth,firestore } from './firebase';
+import { collection, doc, setDoc, query, getDocs } from 'firebase/firestore';
+import { auth, firestore } from './firebase';
 import firebase from 'firebase/compat/app';
+
 const FormComponent = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
+  const [userEmails, setUserEmails] = useState([]);
+  const [selectedUserEmail, setSelectedUserEmail] = useState('');
+
+  useEffect(() => {
+    fetchUserEmails();
+  }, []);
+
+  const fetchUserEmails = async () => {
+    try {
+      const usersCollection = collection(firestore, 'users');
+      const querySnapshot = await getDocs(usersCollection);
+      const emails = querySnapshot.docs.map((doc) => doc.data().email);
+      setUserEmails(emails);
+    } catch (error) {
+      console.error('Error fetching user emails:', error);
+    }
+  };
 
   const toggleForm = () => {
     setIsFormOpen(!isFormOpen);
@@ -31,46 +49,49 @@ const FormComponent = () => {
     setEndDate(e.target.value);
   };
 
-  const handleSubmit =async (e) => {
-    // e.preventDefault();
-    // // Perform form submission logic here
-    // // You can access the form field values (name, description, startDate, endDate) here
-    // console.log('Submitted:', { name, description, startDate, endDate });
-    // // Reset form fields
+  const handleUserEmailChange = (e) => {
+    setSelectedUserEmail(e.target.value);
+  };
 
-    // setName('');
-    // setDescription('');
-    // setStartDate('');
-    // setEndDate('');
-    // setIsFormOpen(false);
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const user=auth.currentUser;
-    console.log("inside handlesubmit");
+    const user = auth.currentUser;
+    console.log('inside handleSubmit');
     try {
       const formData = {
         name: name,
-        email: user.email,
-        ID : user.uid,
+        users_email: selectedUserEmail,
+        admin_email:user.email,
+        ID: user.uid,
         description: description,
-        startDate : startDate,
-        endDate : endDate,
+        startDate: startDate,
+        endDate: endDate,
       };
-  
-      // Get a reference to the Firestore collection named "admins"
-      const adminsCollection = collection(firestore, "admins");
-  
-      // Create a new document with a unique ID
-      const adminDoc = doc(adminsCollection);
-  
+
+      // Get a reference to the Firestore collection named "users"
+      const usersCollection = collection(firestore, 'admins');
+
+      // Create a new document with the logged-in user's ID as the document ID
+      const userDoc = doc(usersCollection, user.uid);
+
+      // Get a reference to the "projects" collection within the user's document
+      const projectsCollection = collection(userDoc, 'projects');
+
+      // Create a new document with a unique ID within the "projects" collection
+      const projectDoc = doc(projectsCollection);
+
       // Set the document data while preserving existing fields
-      await setDoc(adminDoc, formData, { merge: true });
+      await setDoc(projectDoc, formData);
+
       setName('');
       setDescription('');
       setStartDate('');
       setEndDate('');
+      setSelectedUserEmail('');
       setIsFormOpen(false);
+
       // Show success alert
-    alert("Success");
+      alert('Success');
     } catch (error) {
       console.error('Error adding document:', error);
     }
@@ -82,8 +103,22 @@ const FormComponent = () => {
       {isFormOpen && (
         <div className="modal">
           <div className="modal-content">
-            <span className="close" onClick={toggleForm}>&times;</span>
+            <span className="close" onClick={toggleForm}>
+              &times;
+            </span>
             <form onSubmit={handleSubmit}>
+              <label>
+                User Email:
+                <select value={selectedUserEmail} onChange={handleUserEmailChange}>
+                  <option value="">Select an email</option>
+                  {userEmails.map((email, index) => (
+                    <option key={index} value={email}>
+                      {email}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <br />
               <label>
                 Name:
                 <input type="text" value={name} onChange={handleNameChange} />
@@ -114,3 +149,121 @@ const FormComponent = () => {
 };
 
 export default FormComponent;
+
+// import React, { useState } from 'react';
+// import './FormComp.css';
+
+// import { collection, doc, setDoc } from 'firebase/firestore';
+// import { auth, firestore } from './firebase';
+// import firebase from 'firebase/compat/app';
+
+// const FormComponent = () => {
+//   const [isFormOpen, setIsFormOpen] = useState(false);
+//   const [name, setName] = useState('');
+//   const [description, setDescription] = useState('');
+//   const [startDate, setStartDate] = useState('');
+//   const [endDate, setEndDate] = useState('');
+
+//   const toggleForm = () => {
+//     setIsFormOpen(!isFormOpen);
+//   };
+
+//   const handleNameChange = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   const handleDescriptionChange = (e) => {
+//     setDescription(e.target.value);
+//   };
+
+//   const handleStartDateChange = (e) => {
+//     setStartDate(e.target.value);
+//   };
+
+//   const handleEndDateChange = (e) => {
+//     setEndDate(e.target.value);
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const user = auth.currentUser;
+//     console.log('inside handleSubmit');
+//     try {
+//       const formData = {
+//         name: name,
+//         email: user.email,
+//         ID: user.uid,
+//         description: description,
+//         startDate: startDate,
+//         endDate: endDate,
+//       };
+
+//       // Get a reference to the Firestore collection named "users"
+//       const usersCollection = collection(firestore, 'admins');
+
+//       // Create a new document with the logged-in user's ID as the document ID
+//       const userDoc = doc(usersCollection, user.uid);
+
+//       // Get a reference to the "projects" collection within the user's document
+//       const projectsCollection = collection(userDoc, 'projects');
+
+//       // Create a new document with a unique ID within the "projects" collection
+//       const projectDoc = doc(projectsCollection);
+
+//       // Set the document data while preserving existing fields
+//       await setDoc(projectDoc, formData);
+
+//       setName('');
+//       setDescription('');
+//       setStartDate('');
+//       setEndDate('');
+//       setIsFormOpen(false);
+
+//       // Show success alert
+//       alert('Success');
+//     } catch (error) {
+//       console.error('Error adding document:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={toggleForm}>Open Form</button>
+//       {isFormOpen && (
+//         <div className="modal">
+//           <div className="modal-content">
+//             <span className="close" onClick={toggleForm}>
+//               &times;
+//             </span>
+//             <form onSubmit={handleSubmit}>
+//               <label>
+//                 Name:
+//                 <input type="text" value={name} onChange={handleNameChange} />
+//               </label>
+//               <br />
+//               <label>
+//                 Description:
+//                 <textarea value={description} onChange={handleDescriptionChange} />
+//               </label>
+//               <br />
+//               <label>
+//                 Start Date:
+//                 <input type="date" value={startDate} onChange={handleStartDateChange} />
+//               </label>
+//               <br />
+//               <label>
+//                 End Date:
+//                 <input type="date" value={endDate} onChange={handleEndDateChange} />
+//               </label>
+//               <br />
+//               <button type="submit">Submit</button>
+//             </form>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FormComponent;
+
